1ad551f0540be66bc6c51673e4a48525122b6466
vkingmaker <vkingmaker1@gmail.com>
2018-09-27 19:57:31 +0100

    [Feature #160239406] Add a router to enable the user place an order

---
+++ build/src/order.js
@@ -0,0 +1,34 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+
+var _db = require("./mock/db");
+
+var _db2 = _interopRequireDefault(_db);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var db = (0, _db2.default)();
+
+exports.default = {
+  placeOrder: function placeOrder(req, res, next) {
+    var addedOrder = {};
+    if (db.length) {
+      req.body.id = db[db.length - 1].id + 1;
+      addedOrder = req.body;
+      db.push(req.body);
+    } else {
+      req.body.id = 1;
+      addedOrder = req.body;
+      db.push(req.body);
+    }
+
+    res.json({
+      "message": "Your order has been placed!",
+      addedOrder: addedOrder
+    });
+  }
+};
+module.exports = exports.default;
