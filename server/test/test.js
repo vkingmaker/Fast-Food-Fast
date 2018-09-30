b40a040096dbe5b5d992834edbc133fc6a57517e
vkingmaker <vkingmaker1@gmail.com>
2018-09-30 10:32:29 +0100

    [Merge #160840029] Merging ft-delete-by-id-160840029 to develop branch

--- server/test/test.js
+++ server/test/test.js
@@ -15,31 +15,47 @@ describe('Order', () => {
     done();
   });
 
- /*
+    /*
+   * Test the /GET route
+   */
+  describe('/GET order', () => {
+    it('it should GET all the orders', (done) => {
+      chai.request(server)
+        .get('/v1/order')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('array');
+          res.body.length.should.be.eql(5);
+          done();
+        });
+    });
+  });
+
+  /*
    * Test the /DELETE/:id route
    */
-describe('/DELETE/:id order', () => {
-it('it should DELETE an order given the id', (done) => {
-  let order = {
-    "id": 2,
-    "Date": "8/30/2018",
-    "Name": "Ufomba Victoria",
-    "Address": "1b Nweke street Aba, Abia State",
-    "Tel.No": "07061277913",
-    "Order": "Nsala and fufu",
-    "Instruction": "should be served HOT",
-    "status": "Accepted"
-  }
-    chai.request(server)
-      .delete('/v1/order/' + order.id)
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('object');
-        res.body.should.have.property('message').eql(`Order Deleted Successfully id ${order.id}`);
-        done();
-      });
+  describe('/DELETE/:id order', () => {
+    it('it should DELETE an order given the id', (done) => {
+      let order = {
+        "id": 2,
+        "Date": "8/30/2018",
+        "Name": "Ufomba Victoria",
+        "Address": "1b Nweke street Aba, Abia State",
+        "Tel.No": "07061277913",
+        "Order": "Nsala and fufu",
+        "Instruction": "should be served HOT",
+        "status": "Accepted"
+      }
+      chai.request(server)
+        .delete('/v1/order/' + order.id)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql(`Order Deleted Successfully id ${order.id}`);
+          done();
+        });
+    });
   });
-});
   /*
    * Test the /PUT/:id route
    */
@@ -76,72 +92,58 @@ it('it should DELETE an order given the id', (done) => {
         });
     });
   });
-});
 
-/*
- * Test the /GET route
- */
-describe('/GET order', () => {
-  it('it should GET all the orders', (done) => {
-    chai.request(server)
-      .get('/v1/order')
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.body.should.be.a('array');
-        res.body.length.should.be.eql(5);
-        /*
-         * Test the /POST route
-         */
-        describe('/POST order', () => {
-          it('it should POST an Order if the ORDER property has a value', (done) => {
-            let order = {
-              "id": 1,
-              "Date": "8/30/2018",
-              "Name": "Monday Victor Akubudike",
-              "Address": "7b Oyadiran Estate Sabo, Yaba",
-              "Tel.No": "07031977216",
-              "status": "pending"
-            }
-            chai.request(server)
-              .post('/v1/order')
-              .send(order)
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                done();
-              });
-          });
-
-          it('it should POST an order ', (done) => {
-            let order = {
-              "id": 1,
-              "Date": "8/30/2018",
-              "Name": "Monday Victor Akubudike",
-              "Address": "7b Oyadiran Estate Sabo, Yaba",
-              "Tel.No": "07031977216",
-              "Order": "Fried Rice with chicken",
-              "Instruction": "I love my chicken spicy and grilled",
-              "status": "pending"
-            }
-            chai.request(server)
-              .post('/v1/order')
-              .send(order)
-              .end((err, res) => {
-                res.should.have.status(200);
-                res.body.should.be.a('object');
-                res.body.should.have.property('message').eql('Your order has been placed!');
-                res.body.addedOrder.should.have.property('id');
-                res.body.addedOrder.should.have.property('Name');
-                res.body.addedOrder.should.have.property('Address');
-                res.body.addedOrder.should.have.property('Tel.No');
-                res.body.addedOrder.should.have.property('Order');
-                res.body.addedOrder.should.have.property('Instruction');
-                res.body.addedOrder.should.have.property('status');
-                done();
-              });
-          });
+  /*
+   * Test the /POST route
+   */
+  describe('/POST order', () => {
+    it('it should POST an Order if the ORDER property has a value', (done) => {
+      let order = {
+        "id": 1,
+        "Date": "8/30/2018",
+        "Name": "Monday Victor Akubudike",
+        "Address": "7b Oyadiran Estate Sabo, Yaba",
+        "Tel.No": "07031977216",
+        "status": "pending"
+      }
+      chai.request(server)
+        .post('/v1/order')
+        .send(order)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          done();
         });
-      });
+    });
+
+    it('it should POST an order ', (done) => {
+      let order = {
+        "id": 1,
+        "Date": "8/30/2018",
+        "Name": "Monday Victor Akubudike",
+        "Address": "7b Oyadiran Estate Sabo, Yaba",
+        "Tel.No": "07031977216",
+        "Order": "Fried Rice with chicken",
+        "Instruction": "I love my chicken spicy and grilled",
+        "status": "pending"
+      }
+      chai.request(server)
+        .post('/v1/order')
+        .send(order)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.body.should.be.a('object');
+          res.body.should.have.property('message').eql('Your order has been placed!');
+          res.body.addedOrder.should.have.property('id');
+          res.body.addedOrder.should.have.property('Name');
+          res.body.addedOrder.should.have.property('Address');
+          res.body.addedOrder.should.have.property('Tel.No');
+          res.body.addedOrder.should.have.property('Order');
+          res.body.addedOrder.should.have.property('Instruction');
+          res.body.addedOrder.should.have.property('status');
+          done();
+        });
+    });
   });
 
 
